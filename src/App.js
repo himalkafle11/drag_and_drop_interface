12ace299850ec5ed9189b593c192fde6958852ec
himalkafle11 <himalkafle11@gmail.com>
2023-02-09 23:13:33 +0545

    drag and drop done

--- src/App.js
+++ src/App.js
@@ -1,36 +1,26 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 import DragAndDrop from "./components/dragAndDrop";
 
-const data = [
+const givenData = [
   { title: "Column A", childs: ["A1", "A2", "A3"] },
   { title: "Column B", childs: ["B1", "B2", "B3"] },
   { title: "Column C", childs: ["C1", "C2", "C3"] },
 ];
 
 function App() {
+  const [data, setData] = useState();
+  useEffect(() => {
+    if (localStorage.getItem("List")) {
+      console.log(localStorage.getItem("List"));
+      setData(JSON.parse(localStorage.getItem("List")));
+    } else {
+      setData(givenData);
+    }
+  }, [setData]);
   return (
     <div className="App">
       <DragAndDrop data={data} />
-      {/* <header className="appHeader">
-        <div className="dragDrop">
-          {data.map((firstSet) => {
-            return (
-              <div className="dragGroup">
-                <div key={firstSet.title}>
-                  <div className="groupTitle">{firstSet.title}</div>
-                  {firstSet.childs.map((item, index) => {
-                    return (
-                      <div className="dragItem" key={index}>
-                        {item}
-                      </div>
-                    );
-                  })}
-                </div>
-              </div>
-            );
-          })}
-        </div>
-      </header> */}
     </div>
   );
 }
--- src/components/dragAndDrop.js
+++ src/components/dragAndDrop.js
@@ -1,15 +1,99 @@
+import { useRef, useState } from "react";
+
 function DragAndDrop({ data }) {
+  const [list, setList] = useState();
+  const [isDragging, setDragging] = useState(false);
+  const draggedItem = useRef();
+
+  const dragNode = useRef();
+
+  const dragStartHandler = (e, params) => {
+    console.log("drag starting", params);
+    draggedItem.current = params;
+    dragNode.current = e.target;
+    dragNode.current.addEventListener("dragend", dragEndHandler);
+    setTimeout(() => {
+      setDragging(true);
+    }, 0);
+  };
+
+  const dragEnterHandler = (e, params) => {
+    console.log("drag enter", params);
+    const currentItem = draggedItem.current;
+    if (e.target !== dragNode.current) {
+      console.log("same");
+      setList((oldList) => {
+        let newList = JSON.parse(JSON.stringify(oldList));
+        let targetItem = draggedItem.current;
+        newList[currentItem.groupIndex].items.splice(
+          currentItem.itemIndex,
+          0,
+          newList[draggedItem.current.groupIndex].items.splice(
+            draggedItem.current.itemIndex,
+            1
+          )[0]
+        );
+        localStorage.setItem("List", JSON.stringify(newList));
+        return newList;
+      });
+    }
+  };
+
+  const dragEndHandler = () => {
+    console.log("ending drag");
+    setDragging(false);
+    dragNode.current.removeEventListener("dragend", dragEndHandler);
+    draggedItem.current = null;
+    dragNode.current = null;
+  };
+
+  const getStyles = (params) => {
+    const currentItem = draggedItem.current;
+    if (
+      currentItem.groupIndex === params.groupIndex &&
+      currentItem.itemIndex === params.itemIndex
+    ) {
+      return "current dragItem";
+    }
+    return "dragItem";
+  };
+
   return (
     <header className="appHeader">
       <div className="dragDrop">
-        {data.map((firstSet) => {
+        {data.map((firstSet, groupIndex) => {
           return (
-            <div className="dragGroup">
-              <div key={firstSet.title}>
+            <div
+              onDragEnter={
+                isDragging && !firstSet.items.length
+                  ? (e) => dragEnterHandler(e, { groupIndex, itemIndex: 0 })
+                  : null
+              }
+              className="dragGroup"
+            >
+              <div key={groupIndex}>
                 <div className="groupTitle">{firstSet.title}</div>
-                {firstSet.childs.map((item, index) => {
+                {firstSet.childs.map((item, itemIndex) => {
                   return (
-                    <div className="dragItem" key={index}>
+                    <div
+                      draggable
+                      onDragStart={(e) => {
+                        dragStartHandler(e, { groupIndex, itemIndex });
+                      }}
+                      onDragEnter={
+                        isDragging
+                          ? (e) => {
+                              dragEnterHandler(e, { groupIndex, itemIndex });
+                            }
+                          : null
+                      }
+                      className={
+                        isDragging
+                          ? getStyles({ groupIndex, itemIndex })
+                          : "dragItem"
+                      }
+                      key={itemIndex}
+                    >
                       {item}
                     </div>
                   );
