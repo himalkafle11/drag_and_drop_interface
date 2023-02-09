31d64345754db2e73d38a96228340f997725c021
himalkafle11 <himalkafle11@gmail.com>
2023-02-09 16:16:27 +0545

    basic-layout done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import './App.css';
+import "./App.css";
+
+const data = [
+  { title: "Column A", childs: ["A1", "A2", "A3"] },
+  { title: "Column B", childs: ["B1", "B2", "B3"] },
+  { title: "Column C", childs: ["C1", "C2", "C3"] },
+];
 
 function App() {
   return (
     <div className="App">
-      <p>Project started</p>
+      <header className="appHeader">
+        <div className="dragDrop">
+          {data.map((firstSet) => {
+            return (
+              <div className="dragGroup">
+                <div key={firstSet.title}>
+                  <div className="groupTitle">{firstSet.title}</div>
+                  {firstSet.childs.map((item, index) => {
+                    return (
+                      <div className="dragItem" key={index}>
+                        {item}
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </header>
     </div>
   );
 }
